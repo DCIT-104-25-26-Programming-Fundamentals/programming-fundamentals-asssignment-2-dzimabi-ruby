669a3d99859864f2c34fd81a286e85850dbc4984
dzimabi ruby <dzimabiruby@st.ug.edu.gh>
2026-08-01 14:34:50 +0000

    Complete assignment 5: fibonacci sequence

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,59 @@
 #include <iostream>
 using namespace std;
 
+
+#include <iostream>
+using namespace std;
+
+void printFibonacci(int n) {
+    int a = 0, b = 1;
+    cout << "Fibonacci sequence: ";
+    for (int i = 0; i < n; i++) {
+        cout << a;
+        if (i < n - 1) {
+            cout << " ";
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << endl;
+}
+
+bool isFibonacci(int num) {
+    int a = 0, b = 1;
+    while (a <= num) {
+        if (a == num) {
+            return true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    return false;
+}
+
+int main() {
+    int n;
+    cout << "How many terms? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+
+    printFibonacci(n);
+
+    int num;
+    cout << "Enter a number to check: ";
+    cin >> num;
+
+    if (isFibonacci(num)) {
+        cout << num << " is a Fibonacci number." << endl;
+    } else {
+        cout << num << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
